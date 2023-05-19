3528166f21d1a8e929474cb3c6b60b81234f5654
Brendan Alderton <meangraph@gmail.com>
2023-05-19 12:17:07 +1000

    Finally found a way to display relevent data in the customer/sp object.
    Not going to change the existing code its too late now.

--- backend/src/main/java/CSIT3214/GroupProject/Model/Payment.java
+++ backend/src/main/java/CSIT3214/GroupProject/Model/Payment.java
@@ -1,6 +1,7 @@
 package CSIT3214.GroupProject.Model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -18,10 +19,12 @@ public class Payment {
 
     @ManyToOne
     @JoinColumn(name = "customer_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private Customer customer;
 
     @ManyToOne
     @JoinColumn(name = "service_provider_id")
+    @JsonIdentityReference(alwaysAsId = true)
     private ServiceProvider serviceProvider;
 
 
